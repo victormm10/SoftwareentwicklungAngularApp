89815f86dc076772f2752f0d2353402bdd9a7077
Victor Manuel Chacon Melendez <victormanuel230290@gmail.com>
2022-09-08 10:30:59 -0600

    Add angular material, bootstrap and sidebar menu

--- src/app/Products/products.module.ts
+++ src/app/Products/products.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ProductsRoutingModule } from './products-routing.module';
 import { ListComponent } from './Pages/list/list.component';
 import { AddComponent } from './Pages/add/add.component';
+import { ProductsRoutingModule } from './products-routing.module';
 
 
 @NgModule({
