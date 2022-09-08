5641e2530a0748f35068d41b16bb96afddedeb77
Victor Manuel Chacon Melendez <victormanuel230290@gmail.com>
2022-09-08 08:31:03 -0600

    Add lazyload, routing modules and components for products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Softwareentwicklung',
+    loadChildren: () => import('./Products/products.module').then( m => m.ProductsModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'Softwareentwicklung'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
